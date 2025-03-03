4ed193bd87847612f3d648551acb8bf3b6bb30a6
sl1mslav <dm.partilov@gmail.com>
2025-03-03 10:02:57 +0500

    Trying stuff out

--- app/src/main/java/com/sl1mslav/blescanner/blePermissions/BlePermissionsObserver.kt
+++ app/src/main/java/com/sl1mslav/blescanner/blePermissions/BlePermissionsObserver.kt
@@ -3,12 +3,15 @@ package com.sl1mslav.blescanner.blePermissions
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Build
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-typealias Permission = String
+import kotlinx.coroutines.flow.update
+
+private typealias Permission = String
 
 class BlePermissionsObserver(
     private val context: Context
@@ -19,15 +22,22 @@ class BlePermissionsObserver(
     val permissionsState = _permissionsState.asStateFlow()
 
     override fun onResume(owner: LifecycleOwner) {
-
+        _permissionsState.update { calculatePermissionsState() }
     }
 
     private fun calculatePermissionsState(): BlePermissionsState { // todo продумать разницу в версиях
-        return BlePermissionsState(
-            areNotificationsGranted = Manifest.permission.POST_NOTIFICATIONS.isGranted(),
-            areBluetoothPermissionsGranted = false,
-            isBackgroundLocationGranted = false
-        )
+        val notificationsStatus = when {
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> {
+                if (Manifest.permission.POST_NOTIFICATIONS.isGranted())
+                    PermissionStatus.GRANTED
+                else
+                    PermissionStatus.NOT_GRANTED
+            }
+            else -> {
+                PermissionStatus.NOT_NEEDED
+            }
+        }
+
     }
 
     private fun Permission.isGranted(): Boolean {
--- app/src/main/java/com/sl1mslav/blescanner/blePermissions/BlePermissionsState.kt
+++ app/src/main/java/com/sl1mslav/blescanner/blePermissions/BlePermissionsState.kt
@@ -1,7 +1,13 @@
 package com.sl1mslav.blescanner.blePermissions
 
 data class BlePermissionsState(
-    val areNotificationsGranted: Boolean,
-    val areBluetoothPermissionsGranted: Boolean,
-    val isBackgroundLocationGranted: Boolean
+    val notifications: PermissionStatus,
+    val bluetooth: PermissionStatus,
+    val backgroundLocation: PermissionStatus // todo сделать отдельные пермишны на каждые??
 )
+
+enum class PermissionStatus {
+    GRANTED,
+    NOT_GRANTED,
+    NOT_NEEDED
+}
