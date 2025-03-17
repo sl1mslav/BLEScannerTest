253db084c2153f657c241dd583f271f693c03409
sl1mslav <dm.partilov@gmail.com>
2025-03-17 13:26:27 +0500

    Add skud uuid calculation logic

--- app/src/main/java/com/sl1mslav/blescanner/MainActivity.kt
+++ app/src/main/java/com/sl1mslav/blescanner/MainActivity.kt
@@ -55,10 +55,11 @@ class MainActivity : ComponentActivity() {
             val binder = service as BleScannerService.LeScannerBinder
             scannerService = binder.getService()
             viewModel.onChangeServiceState(isRunning = true)
-            val key = "2743652"
+            val key = "2769202"
             val bleCode = "pcjhp6060px38f9b"
+            val uuid = getSkudUuid(skudId = 111383)
             val hardCodedDevice = BleDevice(
-                uuid = "f45389a8-d158-4964-b8ef-00000001ab60",
+                uuid = uuid,
                 keyId = 1,
                 rssi = 0,
                 charData = byteArrayOf(),
@@ -140,9 +141,7 @@ class MainActivity : ComponentActivity() {
 
     private fun onCheckPermission(permission: BlePermission) {
         if (permission.manifestName == ACCESS_BACKGROUND_LOCATION) {
-            multiplePermissionRequester.launch(
-                arrayOf(ACCESS_FINE_LOCATION, ACCESS_BACKGROUND_LOCATION)
-            )
+            permissionRequester.launch(ACCESS_BACKGROUND_LOCATION)
             return
         }
 
@@ -215,7 +214,16 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    private fun getSkudUuid(skudId: Int): String {
+        val skudIdInHex = skudId.toString(radix = HEX_RADIX)
+        val skudIdPart = skudIdInHex.padStart(length = SKUD_ID_HEX_LENGTH, padChar = '0')
+        return SKUD_UUID_PREFIX + skudIdPart
+    }
+
     companion object {
         private const val BIND_SERVICE_IF_RUNNING = 0
+        private const val SKUD_UUID_PREFIX = "f45389a8-d158-4964-b8ef-"
+        private const val HEX_RADIX = 16
+        private const val SKUD_ID_HEX_LENGTH = 12
     }
 }
